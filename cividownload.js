0048f4ca3483ca965511ee42c9155a1cb1da02a5
kurund <kurund@civicrm.org>
2013-11-01 17:47:28 +0530

    more changes / fixes

--- cividownload.js
+++ cividownload.js
@@ -1,22 +1,41 @@
-(function($){
-  $(function() {
-    $( "#slider" ).slider({
-      value:50,
+(function ($) {
+  $(function () {
+    $("#slider").slider({
+      value: 10,
       min: 10,
-      max: 500,
+      max: 200,
       step: 10,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.value );
+      slide: function (event, ui) {
+        $("#amount").val("$" + ui.value);
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+    $("#amount").val("$" + $("#slider").slider("value"));
 
-    var downloadURL = $('#download-link').attr('href');
-    openwindow = window.open(downloadURL, 'pop-id','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=640,height=420,left = 202,top = 184');
+    $(".crm-input").click(function () {
+      var downloadURL = $('#download-link').attr('href');
+      openwindow = window.open(downloadURL, 'pop-id','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=640,height=420,left = 202,top = 184');
 
-    $(openwindow).load(function(){
-      window.parent.opener.focus();
+      $(openwindow).load(function(){
+        window.opener.parent.focus();
+      });
+
+      if ($('#download-donate').attr('checked')) {
+        // redirect to paypal for donation
+        console.log('redirect to paypal');
+      }
+
+      $('.crm-download-info').removeClass('crm-hidden');
+      $('.crm-download-content').addClass('crm-hidden');
     });
+
+    /*
+     var downloadURL = $('#download-link').attr('href');
+     openwindow = window.open(downloadURL, 'pop-id','toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=640,height=420,left = 202,top = 184');
+
+     $(openwindow).load(function(){
+     window.parent.opener.focus();
+     });
+     */
 
   });
 }(jQuery));
--- cividownload.tpl.php
+++ cividownload.tpl.php
@@ -1,23 +1,50 @@
 <?php if (isset($content['listing'])) { ?>
-  <div class='introduction'>
-    This is an introductory text for listing
-  </div>
+  <div class="crm-title">CiviCRM Downloads</div>
+  <div class='crm-support'>
+    <div class='introduction'>
+      Below is the list of available downloads:
+    </div>
 
-  <ul name="download-links">
-    <?php foreach ($content['download_urls'] as $key => $values) { ?>
-      <li><a href="/cividownload/download/<?php echo $key; ?>"><?php echo $values['title']; ?></a></li>
-    <?php } ?>
-  </ul>
+    <ul name="download-links">
+      <?php foreach ($content['download_urls'] as $key => $values) { ?>
+        <li><a href="/cividownload/<?php echo $key; ?>"><?php echo $values['title']; ?></a></li>
+      <?php } ?>
+    </ul>
+  </div>
 <?php } elseif (isset($content['download'])) { ?>
 
-  <div class='introduction'>
+  <div class="crm-title">Downloading...</div>
+  <div class='crm-support'>
+    <div class="crm-download-info crm-hidden">
+      <div>Thanks for downloading</div>
+      <div class="down-link">If the download doesn't start in a few seconds, please <a
+          href="<?php echo $content['downloadURL']; ?>" id="download-link" target="_blank">click here</a> to start
+        download.
+      </div>
+    </div>
+    <div class="crm-download-content">
+      <div class="crm-download-intro">
+        CiviCRM - Members Supported Open Source Software. Please consider to donate and support it's continuous growth.
+      </div>
+      <p>
+        <label for="amount">Donation amount: ($10 increments)</label>
+        <input type="text" id="amount" class="crm-amount" readonly/>
+      </p>
+
+      <div id="slider"></div>
+
+      <div class="crm-donate-option">
+        <input type="checkbox" class="" id="download-donate" value="1" checked> Yes, I want to donate above amount to support the project.
+      </div>
+      <div class="crm-buttons">
+<!--      <span class="donate-download">
+        <input type="submit" class="crm-input" name="download-donate" value="Donate & Download">
+      </span>-->
+      <span class="download-only">
+        <input type="submit" class="crm-input" name="download-only" value="Download">
+      </span>
+      </div>
+      <br/>
+    </div>
   </div>
-  <div class="down-link">If the download doesn't start in a few seconds, please <a href="<?php echo $content['downloadURL'];?>" id="download-link" target="_blank">click here</a> to start download.</div>
-
-  <p>
-    <label for="amount">Donation amount: ($10 increments)</label>
-    <input type="text" id="amount" style="border: 0; color: #f6931f; font-weight: bold;" />
-  </p>
-
-  <div id="slider"></div>
 <?php } ?>
