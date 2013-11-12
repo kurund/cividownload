a864bfc550371f9f69b61c6643f9000e065532a0
kurund <kurund@civicrm.org>
2013-11-12 19:07:56 +0530

    more fixes

--- cividownload.tpl.php
+++ cividownload.tpl.php
@@ -2,8 +2,8 @@
   <div class="crm-title">Download CiviCRM</div>
   <div class='crm-support'>
     <div class='crm-introduction'>
-      The current stable version of CiviCRM is 4.4. You will need to know which host Content Management software (CMS) and which version you are using.
-      CiviCRM 4.4 is compatible with Drupal version 7.x, Joomla! version 2.5.x / 3.x, and WordPress version 3.4.x. There is a separate download for Drupal
+      The current stable version of CiviCRM is <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?>. You will need to know which host Content Management software (CMS) and which version you are using.
+      CiviCRM <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?> is compatible with Drupal version 7.x, Joomla! version 2.5.x / 3.x, and WordPress version 3.4.x. There is a separate download for Drupal
       version 6.x which is not officially supported. Click on your CMS below to begin.
     </div>
 
