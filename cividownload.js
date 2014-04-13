ef28288c293569e1a3e572d3caa3b9571299cd0d
kurund <kurund@civicrm.org>
2014-04-12 23:57:09 -0700

    changes to download module to use contribution page

--- cividownload.js
+++ cividownload.js
@@ -1,16 +1,22 @@
 (function ($) {
   $(function () {
-    $("#slider").slider({
-      value: 50,
-      min: 10,
-      max: 200,
-      step: 10,
-      slide: function (event, ui) {
-        $("#amount").val(ui.value);
-      }
-    });
+    if ($("#slider").length) {
+      $("#slider").slider({
+        value: 250,
+        min: 10,
+        max: 1000,
+        step: 10,
+        slide: function (event, ui) {
+          $('.other_amount-content input').val(ui.value);
+        }
+      });
 
-    $("#amount").val($("#slider").slider("value"));
+      $('.other_amount-content input').val($("#slider").slider("value"));
+
+      $('.other_amount-section label').remove();
+      $('#priceset-div').addClass('crm-other-amount');
+      $('#_qf_Main_upload-bottom').val('Donate');
+    }
   });
 }(jQuery));
 
--- cividownload.tpl.php
+++ cividownload.tpl.php
@@ -1,52 +1,17 @@
-<?php if (isset($content['listing'])) { ?>
-  <div class="crm-title">Download CiviCRM</div>
-  <div class='crm-support'>
-    <div class='crm-introduction'>
-      The current stable version of CiviCRM is <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?>. You will need to know which host Content Management software (CMS) and which version you are using.
-      CiviCRM <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?> is compatible with Drupal version 7.x, Joomla! version 2.5.x / 3.x, and WordPress version 3.4.x. There is a separate download for Drupal
-      version 6.x which is not officially supported. Click on your CMS below to begin.
-    </div>
+<div class="crm-title">Download CiviCRM</div>
+<div class='crm-support'>
+  <div class='crm-introduction'>
+    The current stable version of CiviCRM is <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?>. You will
+    need to know which host Content Management software (CMS) and which version you are using.
+    CiviCRM <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?> is compatible with Drupal version 7.x,
+    Joomla! version 2.5.x / 3.x, and WordPress version 3.4.x. There is a separate download for Drupal
+    version 6.x which is not officially supported. Click on your CMS below to begin.
+  </div>
 
-    <ul name="download-links">
-      <?php foreach ($content['download_urls'] as $key => $values) { ?>
-        <li><a href="<?php echo url('cividownload/' .$key); ?>"><?php echo 'CiviCRM for ' . $values['title']; ?></a></li>
-      <?php } ?>
-    </ul>
-  </div>
-<?php } elseif (isset($content['download'])) { ?>
-  <div class="crm-title">Downloading...</div>
-  <div class='crm-support'>
-    <div class="crm-download-content">
-      <div class="crm-download-intro">
-        CiviCRM is Member-supported Open Source Software. Please consider contributing to support ongoing improvements to the project.
-      </div>
-      <div class="down-link">Thanks for downloading. If you were not able to download <a
-          href="<?php echo $content['downloadURL']; ?>" id="download-link" target="_blank">click here</a> to start the
-        download.
-      </div>
-      <p>
-        <div>(move slider to change amount)</div>
-      </p>
-      <div class="slider-container">
-        <div id="slider"></div>
-      </div>
-      <div class="crm-buttons">
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_donations">
-          <input type="hidden" name="business" value="dave@civicrm.org">
-          <input type="hidden" name="lc" value="US">
-          <input type="hidden" name="currency_code" value="USD">
-          <input type="hidden" name="no_note" value="0">
-          <div class="crm-button-layout">
-            <div class="crm-button-inner-layout">
-              <input type="submit" class="crm-input" name="download-donate" value="Donate">
-              <input type="text" name="amount" id="amount" class="crm-amount"/><span class="crm-currency">$</span>
-            </div>
-          </div>
-        </form>
-      </div>
-      <br/>
-      <iframe src="<?php //echo $content['downloadURL'];?>" class="crm-hidden"></iframe>
-    </div>
-  </div>
-<?php } ?>
+  <ul name="download-links">
+    <?php foreach ($content['download_urls'] as $key => $values) { ?>
+      <li><a class="download-link" href="<?php echo $values['url']; ?>"
+             filename="<?php echo $values['filename']; ?>"><?php echo 'CiviCRM for ' . $values['title']; ?></a></li>
+    <?php } ?>
+  </ul>
+</div>
