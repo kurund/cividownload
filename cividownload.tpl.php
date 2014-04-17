f5e5a253d747f08d9d95fb279f7ea6792f18e9e3
kurund <kurund@civicrm.org>
2014-04-17 15:30:13 -0700

    more fixes based on the feedback from -planning

--- cividownload.tpl.php
+++ cividownload.tpl.php
@@ -1,10 +1,11 @@
 <div class="crm-download-title">Download CiviCRM</div>
 <div class='crm-support'>
   <div class='crm-introduction'>
-    The current stable version of CiviCRM is <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?>. You will
+    <?php $civicrm_version = variable_get('civicrm_stable_version', '4.4.0'); ?>
+    The current stable version of CiviCRM is <?php echo $civicrm_version; ?>. You will
     need to know which host Content Management software (CMS) and which version you are using.
-    CiviCRM <?php echo variable_get('civicrm_stable_version', '4.4.0'); ?> is compatible with Drupal version 7.x,
-    Joomla! version 2.5.x / 3.x, and WordPress version 3.4.x. There is a separate download for Drupal
+    CiviCRM <?php echo $civicrm_version; ?> is compatible with Drupal version 7.x,
+    Joomla! version 2.5.x / 3.x, and WordPress 3.x. There is a separate download for Drupal
     version 6.x which is supported by the community. Click on your CMS below to begin.
   </div>
 
@@ -13,8 +14,8 @@
       <div class="crm-download-buttons">
         <a class="download-link"
            href="<?php echo $values['url']; ?>"
-           filename="<?php echo $values['filename']; ?>">Download</a>
-        &nbsp;<strong><?php echo 'CiviCRM for ' . $values['title']; ?></strong>
+           filename="<?php echo $values['filename']; ?>">Download CiviCRM <?php echo $civicrm_version; ?></a>
+        &nbsp;<strong><?php echo 'for ' . $values['title']; ?></strong>
       </div>
       <br/>
     <?php } ?>
