0b643de99bf38eb0f0488afffc62ab59678c57fd
kurund <kurund@civicrm.org>
2014-11-05 02:56:09 +0530

    modified to support lts download

--- cividownload.tpl.php
+++ cividownload.tpl.php
@@ -1,4 +1,4 @@
-<?php $civicrm_version = variable_get('civicrm_stable_version', '4.4.0'); ?>
+<?php $civicrm_version = variable_get('civicrm_stable_version', '4.5.2'); ?>
 <div class="crm-download-title">CiviCRM <?php echo $civicrm_version; ?></div>
 <div class='crm-support'>
   <div class='crm-introduction'>
@@ -15,16 +15,18 @@
     <?php } ?>
   </div>
   <!-- LTS section -->
-      <div class="crm-download-title">CiviCRM 4.4.9 LTS</div>
+
+  <?php $civicrm_lts_version = variable_get('civicrm_lts_version', '4.4.10'); ?>
+<div class="crm-download-title">CiviCRM <?php echo $civicrm_lts_version; ?> LTS</div>
   <div class="crm-support">
     <div class="crm-introduction">
-          The current Long Term Support (LTS) release CiviCRM is 4.4.9. Select a download based on the content management software (CMS) you are using.
+    The current Long Term Support (LTS) release CiviCRM is <?php echo $civicrm_lts_version; ?>. Select a download based on the content management software (CMS) you are using.
     </div>
     <div class="crm-download-listing">
     <?php foreach ($content['download_urls'] as $key => $values) { ?>
       <div class="crm-download-buttons">
-        <a class="download-link" href="<?php echo $values['url'];?>">Download</a>
-        &nbsp;CiviCRM <?php echo $civicrm_version; ?> <?php echo 'for ' . $values['title']; ?></strong>
+        <a class="download-link" href="<?php echo $values['url'];?>&rtype=lts">Download</a>
+        &nbsp;CiviCRM <?php echo $civicrm_lts_version; ?> <?php echo 'for ' . $values['title']; ?></strong>
       </div>
       <br/>
     <?php } ?>
